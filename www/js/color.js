c0030f44130f351b11d1fd383325f250df93de6d
System Administrator <gwen.bischoff@hotmail.com>
2016-06-05 11:14:01 +0200

    More layout changes

--- www/js/color.js
+++ www/js/color.js
@@ -1,7 +1,8 @@
 var temp = 28;
 
 if (temp == 28){
-	document.querySelector("header").style.borderColor = "#e1a81f";
+	document.querySelector("#navDiv").style.borderColor = "#e1a81f";
 	document.querySelector(".sideNav").style.borderColor = "#e1a81f";
-
+	document.querySelector(".logo2").style.color = "#e1a81f";
+	document.querySelector("footer").style.backgroundColor = "#e1a81f";
 }
--- www/js/slider.js
+++ www/js/slider.js
@@ -1,15 +1,33 @@
+var red = 0;
+var green = 0;
+var blue = 0;
+
 function showValueRed(newValue)
 {
-    document.getElementById("textRangeRed").innerHTML=newValue;
+    red = newValue;
+    document.getElementById("textRangeRed").innerHTML = red;
+    changeColor();
 }        
 
 function showValueGreen(newValue)
 {
-    document.getElementById("textRangeGreen").innerHTML=newValue;
+    green = newValue;
+    document.getElementById("textRangeGreen").innerHTML = green;
+    changeColor();
 }        
 
 function showValueBlue(newValue)
 {
-    document.getElementById("textRangeBlue").innerHTML=newValue;
+    blue = newValue;
+    document.getElementById("textRangeBlue").innerHTML = blue;
+    changeColor();
 }
+
+function changeColor(){
+	document.querySelector("#navDiv").style.borderColor = "rgb("+red+","+green+","+blue+")";
+	document.querySelector(".sideNav").style.borderColor = "rgb("+red+","+green+","+blue+")";
+	document.querySelector(".logo2").style.color = "rgb("+red+","+green+","+blue+")";
+	document.querySelector("footer").style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+}
+
 
